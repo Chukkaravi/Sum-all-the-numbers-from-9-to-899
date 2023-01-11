08d68559158f6d0a07a3b58b5f7560d82e8e84e6
Chukka Venkata Ravi <102050741+Chukkaravi@users.noreply.github.com>
2023-01-11 22:12:16 +0530

    Add files via upload

---
+++ questionSum.java
@@ -0,0 +1,12 @@
+public class questionSum{
+    public static void main(String[] args){
+        int sum = 0;
+        int n = 899;
+
+        for (int i = 9 ; i <=n ; ++i){
+            sum += i;
+        }
+        System.out.println("Sum = "+ sum);
+
+    }
+}
